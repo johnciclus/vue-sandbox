6343bc89adab65fd0a4db1ee4372178ecd917d5d
John Garavito Suarez <jfreddy@br.ibm.com>
2017-09-08 23:31:32 -0300

    Adding counter component

---
+++ public/components/ui-counter.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h2>The count is {{counter}}</h2>
+
+        <button class="ui button"  @click="counter++"></button>
+
+    </div>
+</template>
+
+<script type="text/javascript">
+    (function() {
+        "use strict";
+        module.exports = {
+            "props": [],
+            "data": function(){
+                return{
+                    counter: 0;
+                }
+            },
+            "methods": {
+            }
+        };
+    }());
+</script>
